9fcf9864428bc8de841a654aea8aedd36ce523a6
Kiong <47136221+MazKionkTea@users.noreply.github.com>
2026-08-04 06:30:04 +0700

    Update nmap.py

--- nmap.py
+++ nmap.py
@@ -1,1 +1,13 @@
-imprort help
+import help
+
+import subprocess
+
+nmap = subprocess.Popen("nmap")
+target = input(help.target, "target : ")
+teknik = input("-sS, -sT, -sU, -sF\nteknik pemindaian port: ")
+port = input("spesifikasi port : ")
+deteksi = input("deteksi layanan & os : ")
+NSE = input()
+waktu = input()
+eva_spoof = input()
+output = input()
